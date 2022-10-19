57afe9daf5f207311afd1161da7a98f7e30df534
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-10-19 16:29:10 +1300

    Added player jumping on spacebar press

--- Assets/TinyRobot/Code/Scripts/PlayerMovement.cs
+++ Assets/TinyRobot/Code/Scripts/PlayerMovement.cs
@@ -2,13 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
   public float speed;
+  public float jumpSpeed;
+  public Rigidbody rb;
+  private enum state
+  {
+    ready,
+    jumping,
+    doubleJumping,
+  }
 
-  void Update() {
+  private void jump()
+  {
+    rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+  }
+
+  void Update()
+  {
     //float vertical = Input.GetAxisRaw("Vertical");
     float horizontal = Input.GetAxisRaw("Horizontal");
 
+    // FIXME: Capsule falls off the platform when you move close to the edge.
     gameObject.transform.position = new Vector2(transform.position.x + (horizontal * speed), transform.position.y);
+
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      jump();
+    }
   }
 }
