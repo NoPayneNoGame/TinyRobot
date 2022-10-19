858dbe1f8c36e22cf16edaa39cb558350710c029
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-10-19 17:23:49 +1300

    Small refactor and added basic(bad) double jumping

--- Assets/TinyRobot/Code/Scripts/PlayerMovement.cs
+++ Assets/TinyRobot/Code/Scripts/PlayerMovement.cs
@@ -7,29 +7,83 @@ public class PlayerMovement : MonoBehaviour
   public float speed;
   public float jumpSpeed;
   public Rigidbody rb;
+
   private enum state
   {
     ready,
     jumping,
     doubleJumping,
+    falling,
   }
+  private state currentState = state.ready;
+  private bool spacePressed = false;
 
   private void jump()
   {
-    rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+    switch (currentState)
+    {
+      case state.ready:
+        rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+        currentState = state.jumping;
+        break;
+      case state.jumping:
+      case state.falling:
+        if (!spacePressed)
+        {
+          currentState = state.doubleJumping;
+          // FIXME: Add flutter and remove downward force
+          rb.AddForce(Vector3.up * (jumpSpeed / 2), ForceMode.Impulse);
+        }
+        break;
+    }
   }
+
+  private void teleport(Vector3 target)
+  {
+    gameObject.transform.position = target;
+  }
+
+  private void move(float horizontalAxis)
+  {
+    // FIXME: Should probably use forces/velocity instead of just teleporting the player a little left/right.
+    teleport(new Vector3(transform.position.x + (horizontalAxis * speed), transform.position.y, transform.position.z));
+  }
+
+  void OnCollisionEnter(Collision hit)
+  {
+    if (hit.gameObject.tag == "Floor")
+    {
+      currentState = state.ready;
+    }
+  }
+
+  void OnCollisionExit(Collision hit)
+  {
+    if (hit.gameObject.tag == "Floor")
+    {
+      if (currentState == state.ready)
+      {
+        currentState = state.falling;
+      }
+    }
+  }
+
 
   void Update()
   {
-    //float vertical = Input.GetAxisRaw("Vertical");
     float horizontal = Input.GetAxisRaw("Horizontal");
 
-    // FIXME: Capsule falls off the platform when you move close to the edge.
-    gameObject.transform.position = new Vector2(transform.position.x + (horizontal * speed), transform.position.y);
+    move(horizontal);
 
     if (Input.GetKeyDown(KeyCode.Space))
     {
       jump();
+      spacePressed = true;
+    }
+
+    if (Input.GetKeyUp(KeyCode.Space))
+    {
+      spacePressed = false;
     }
   }
 }
